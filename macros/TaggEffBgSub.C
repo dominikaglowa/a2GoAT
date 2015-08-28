c93850775d380d1e4fb90ea293296d8ca5b2549e
Dominika Glowa <s0678212@strappi.ph.ed.ac.uk>
2015-08-28 16:52:54 +0100

    add macro

--- macros/TaggEffBgSub.C
+++ macros/TaggEffBgSub.C
@@ -1,6 +1,6 @@
 void TaggEffBgSub(TString sBeam, TString sBkg1, TString sBkg2="", Bool_t bFreeScal=false){
-  TFile fBeam(sBeam,"READ");
-  TFile fBkg1(sBkg1,"READ");  
+  TFile fBeam(sBeam.Data(),"READ");
+  TFile fBkg1(sBkg1.Data(),"READ");  
   gROOT->cd();
 
   TH1D *hBeamAllHits = (TH1D*)fBeam.Get("TaggerAllHits");
@@ -12,6 +12,8 @@ void TaggEffBgSub(TString sBeam, TString sBkg1, TString sBkg2="", Bool_t bFreeSc
   Double_t dBeamClock = hBeamLiveTime->GetBinContent(hBeamLiveTime->GetXaxis()->FindBin("Clock"));
   Double_t dBeamInhib = hBeamLiveTime->GetBinContent(hBeamLiveTime->GetXaxis()->FindBin("Inhibited"));
 
+  cout << "Clock=" << dBeamClock << "   Inhib=" << dBeamInhib << endl;
+
   TH1D *hBkg1AccScal = (TH1D*)fBkg1.Get("TaggerAccScal");
   TH1D *hBkg1LiveTime = (TH1D*)fBkg1.Get("LiveTimeScal");
 
@@ -21,7 +23,7 @@ void TaggEffBgSub(TString sBeam, TString sBkg1, TString sBkg2="", Bool_t bFreeSc
   Double_t dBackInhib = hBkg1LiveTime->GetBinContent(hBkg1LiveTime->GetXaxis()->FindBin("Inhibited"));
   
   if(sBkg2 != ""){
-    TFile fBkg2(sBkg2,"READ");
+    TFile fBkg2(sBkg2.Data(),"READ");
     gROOT->cd();
 
     TH1D *hBkg2AccScal = (TH1D*)fBkg2.Get("TaggerAccScal");
@@ -33,42 +35,30 @@ void TaggEffBgSub(TString sBeam, TString sBkg1, TString sBkg2="", Bool_t bFreeSc
     dBackInhib += hBkg2LiveTime->GetBinContent(hBkg2LiveTime->GetXaxis()->FindBin("Inhibited"));
   }
 
-  TString sOut = sBeam;
-  sOut.ReplaceAll("GoAT","BkgSub");
+  cout << "Bcg Clock=" << dBackClock << "   Gcg Inhib=" << dBackInhib << endl;
+  TFile fOut("TaggEff_sn124.root","RECREATE");
 
-  TFile fOut(sOut,"RECREATE");
-
-  TH1D *hEffAllHits = (TH1D*)hBeamAllHits->Clone("TaggEffAllHits");
-  TH1D *hEffSingles = (TH1D*)hBeamSingles->Clone("TaggEffSingles");
-  TH1D *hEffDoubles = (TH1D*)hBeamDoubles->Clone("TaggEffDoubles");
-  TH1D *hEffAccScal = (TH1D*)hBeamAccScal->Clone("TaggEffAccScal");
+  TH1D *hEffAllHits = (TH1D*)hBeamAllHits->Clone("hEffAllHits");
+  TH1D *hEffAllHits2 = (TH1D*)hBeamAllHits->Clone("hEffAllHits2");
+  TH1D *hEffSingles = (TH1D*)hBeamSingles->Clone("hEffSingles");
+  TH1D *hEffDoubles = (TH1D*)hBeamDoubles->Clone("hEffDoubles");
+  TH1D *hEffAccScal = (TH1D*)hBeamAccScal->Clone("hEffAccScal");
+  TH1D *hEffAccScal2 = (TH1D*)hBeamAccScal->Clone("hEffAccScal2");
+  TH1D *hBckgAccScal = (TH1D*)hBackAccScal->Clone("hBckgAccScal");
   hEffAccScal->Sumw2();
+  hEffAccScal2->Sumw2();
 
   if(bFreeScal){
     hEffAccScal->Add(hBackAccScal,(-dBeamClock/dBackClock));
     hEffAccScal->Scale(dBeamInhib/dBeamClock);
   }
-  else hEffAccScal->Add(hBackAccScal,(-dBeamInhib/dBackInhib));
+  else hEffAccScal->Add(hBackAccScal,(-dBeamInhib/dBackClock));
 
   hEffAllHits->Divide(hEffAccScal);
   hEffSingles->Divide(hEffAccScal);
   hEffDoubles->Divide(hEffAccScal);
-
-  Double_t dMean = 0;
-  Int_t iMean = 0;
-  for(Int_t i=0; i<hEffAllHits->GetNbinsX(); i++){
-    if(hEffAllHits->GetBinContent(i+1) > 0){
-      dMean += hEffAllHits->GetBinContent(i+1);
-      iMean++;
-    }
-  }
-
-  dMean = 0.015*TMath::Nint(100*dMean/iMean);
-
-  hEffAllHits->GetYaxis()->SetRangeUser(0,dMean);
-  hEffSingles->GetYaxis()->SetRangeUser(0,dMean);
-  hEffDoubles->GetYaxis()->SetRangeUser(0,dMean);
-  
+  hEffAllHits2->Divide(hEffAccScal2);
+		     
   fOut.Write();
   fOut.Close();
 }
